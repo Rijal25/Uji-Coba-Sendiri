786c3bfc4d770a2894e3448e7eafa8a78d392931
Rizal Mila Pambudi <107825914+Rijal25@users.noreply.github.com>
2022-07-14 11:19:37 +0700

    uji coba sendiri biodata sederhana
    sumber dari youtube

---
+++ biodata sederhana.py
@@ -0,0 +1,21 @@
+# import getpass
+
+# print('Program Login Sederhana')
+# print('-----------------------')
+
+# username_1 = input('Buat username baru anda : ')
+# password_1 = input('Buat password baru anda : ')
+# print('\n-----------DATA DIKONFIRMASI-----------')
+
+# username_2 = input('\nMasukkan username anda : ')
+# password_2 = getpass.getpass('Masukkan kata sandi : ')
+
+# if username_2==username_1 and passsword_2==password_1:
+#     print('----------------------------------------------')
+#     print('Login diterima, selamat datang ' + username_1 + '..')
+#     print('----------------------------------------------')
+# else:
+#     print('----------------------------------------------')
+#     print('Login ditolak, silahkan coba lagi! ')
+#     print('----------------------------------------------')
+    
